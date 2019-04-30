bebd14095baa976031981d18bbc3ce35cc119cf9
Robert Tate <btate712@gmail.com>
2019-04-30 00:53:57 +0000

    stubbed out notes controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,26 @@
+class NotesController < ApplicationController
+
+  get '/notes/new' do
+
+  end
+
+  post '/notes/new' do
+
+  end
+
+  get '/notes/:id' do
+
+  end
+
+  get '/notes/:id/edit' do
+
+  end
+
+  patch '/notes/:id/edit' do
+
+  end
+
+  get '/notes/:id/delete' do
+
+  end
+end
--- config.ru
+++ config.ru
@@ -5,6 +5,7 @@ if ActiveRecord::Base.connection.migration_context.needs_migration?
 end
 
 use Rack::MethodOverride
+use NotesController
 use UsersController
 use TasksController
 run ApplicationController
